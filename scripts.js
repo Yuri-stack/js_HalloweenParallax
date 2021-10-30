e71d1d78e01d2351444221e632f4a8e89b2e17b1
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-10-30 13:02:49 -0300

    Feat.: Finalizando o Projeto

---
+++ scripts.js
@@ -0,0 +1,20 @@
+gsap.to("#img-witch", {
+    scrollTrigger: { scrub: 0.5 },
+    x: 1500,
+    y: 500
+})
+
+gsap.from("#img-wolf", {
+    scrollTrigger: { scrub: 0.5 },
+    x: 400
+})
+
+gsap.from("#img-castle", {
+    scrollTrigger: { scrub: 0.5 },
+    x: -100
+})
+
+gsap.from("#img-bats", {
+    scrollTrigger: { scrub: 0.5 },
+    scale: 1.5
+})
